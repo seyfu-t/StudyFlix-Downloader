06f5448113e779924261bfad9e5dbc1e6bc8c7f2
s-talay <seyfullahtalay@outlook.de>
2023-12-16 03:54:27 +0100

    Proof of Concept Code

--- downloader.py
+++ downloader.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import subprocess
 import argparse
 import sys
+import os
 
 # Audio:        ffmpeg -analyzeduration 100M -probesize 100M -i "https://ducsmxsiuk0zw.cloudfront.net/video_1325/1142/1142.m3u8" -vn -ar 48000 -ac 2 -c:a copy audio.aac
 # Video:        ffmpeg -i "https://ducsmxsiuk0zw.cloudfront.net/video_1325/1142/1142.m3u8" -an -c:v copy video.mp4
@@ -18,15 +19,54 @@ def download_video(url, m3u8_selector, title_selector):
     m3u8_url = soup.select_one(m3u8_selector)['content']
     title = soup.select_one(title_selector).text.strip().replace(' ', '_')
 
-    ffmpeg_command = [
-        'ffmpeg',
-        '-http_proxy', 'socks5://localhost:25344',  # Uncomment to use a proxy
-        '-i', m3u8_url,
-        '-c', 'copy',
-        f'{title}.mp4'
-    ]
+    #ffmpeg_command = [
+    #    'ffmpeg',
+    #    '-http_proxy', 'socks5://localhost:25344',  # Uncomment to use a proxy
+    #    '-i', m3u8_url,
+    #    '-c', 'copy',
+    #    f'{title}.mp4'
+    #]
 
-    subprocess.run(ffmpeg_command)
+    ffmpeg_audio=[
+        "ffmpeg",
+        "-analyzeduration","100M",
+        "-probesize","100M",
+        "-i",m3u8_url,
+        "-vn",
+        "-ar","48000",
+        "-ac","2",
+        "-c:a","copy",
+        "cache/"+title+".aac"
+    ]
+    ffmpeg_video=[
+        "ffmpeg",
+        "-i",m3u8_url,
+        "-an",
+        "-c:v","copy",
+        "cache/"+title+".mp4"
+    ]
+    ffmpeg_combine=[
+        "ffmpeg",
+        "-i","cache/"+title+".mp4",
+        "-i","cache/"+title+".aac",
+        "-c:v","copy",
+        "-c:a","copy",
+        "-shortest",
+        "final/"+title+".mp4"
+    ]
+    if not os.path.exists("./cache"):
+        os.makedirs("./cache")
+    if not os.path.exists("./final"):
+        os.makedirs("./final")
+
+    print(ffmpeg_audio)
+    print(ffmpeg_video)
+    print(ffmpeg_combine)
+
+    subprocess.run(ffmpeg_audio)
+    subprocess.run(ffmpeg_video)
+    subprocess.run(ffmpeg_combine)
+
 
 def process_file(file_path, m3u8_selector, title_selector):
     with open(file_path, 'r') as file:
@@ -35,9 +75,11 @@ def process_file(file_path, m3u8_selector, title_selector):
             if url:
                 download_video(url, m3u8_selector, title_selector)
 
+
 def main():
     parser = argparse.ArgumentParser(description='Download videos from URLs.')
-    parser.add_argument('--file', '-f', type=str, help='Path to a file with URLs, one per line.')
+    parser.add_argument('--file', '-f', type=str,
+                        help='Path to a file with URLs, one per line.')
     parser.add_argument('urls', nargs='*', help='Direct URL input')
 
     args = parser.parse_args()
@@ -50,6 +92,7 @@ def main():
 
     for url in args.urls:
         download_video(url, m3u8_sel, title_sel)
+
 
 if __name__ == '__main__':
     main()
